55eb4e89df924f9d8da7384ec4bd088614c42a5d
hardiikb <hardikbabariya1749@gmail.com>
2019-08-25 17:24:16 -0400

    final commit

--- hbabariy_Lab1/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const MarketPlace = artifacts.require("../contracts/MarketPlace.sol");
-
-module.exports = function(deployer) {
-	deployer.deploy(MarketPlace);
-};
--- hbabariy_Lab1/src/app/heroes/extras.ts
+++
@@ -1,1 +0,0 @@
-export const extras: string[] = ["vilas", "shailesh", "navneet", "indu"];
